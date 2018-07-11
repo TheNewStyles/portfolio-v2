935769130da6ccc14801adb256bf4534da143b50
TheNewStyles <tyler.peterson@utah.edu>
2018-07-11 10:15:50 -0500

    Add semantic html5 outline to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            <header>
+                <nav id="nav">
+                    {/* should always be on top of viewport */}
+                    <ul>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+          <main>
+            <section id="welcome-section">
+                <h1></h1>
+                {/* Should be height of viewport */}
+            </section>
+            <section id="projects">
+                <article className="project-tile" ></article>
+                <article className="project-tile" ></article>
+                <article className="project-tile" ></article>
+            </section>
+            <section>
+
+            </section>
+          </main>
+          <footer>
+              <ul>
+                  <li id="profile-link"></li>
+              </ul>
+          </footer>
       </div>
     );
   }
