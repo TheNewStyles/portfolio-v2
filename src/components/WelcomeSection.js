c0d066a2d5d159436d563ffa1ef38d2df13d21ab
TheNewStyles <tyler.peterson@utah.edu>
2018-07-14 08:24:46 -0500

    fix nav bar

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -2,23 +2,50 @@ import React, { Component } from 'react';
 import '../styles/WelcomeSection.css';
 
 class WelcomeSection extends Component {
-  render() {
-    return (
-        <section id="welcome-section">
-            <div className="welcome-container">
-                <h1>TYLER PETERSON</h1>
-                <h3>DEVELOPER</h3>
-                <h3>DESIGNER</h3>
-                <h3>TESTER</h3>
-            </div>
-            <div className="scroll-downs">
-                <div className="mousey">
-                    <div className="scroller"></div>
+    generateNoise(opacity) {
+        if ( !!!document.createElement('canvas').getContext ) {
+           return false;
+        }
+
+        var canvas = document.createElement("canvas"),
+        ctx = canvas.getContext('2d'),
+        x, y,
+        number,
+        opacity = opacity || .2;
+
+        canvas.width = 45;
+        canvas.height = 45;
+
+        for ( x = 0; x < canvas.width; x++ ) {
+           for ( y = 0; y < canvas.height; y++ ) {
+              number = Math.floor( Math.random() * 60 );
+
+              ctx.fillStyle = "rgba(" + number + "," + number + "," + number + "," + opacity + ")";
+              ctx.fillRect(x, y, 1, 1);
+           }
+        }
+
+        document.body.style.backgroundImage = "url(" + canvas.toDataURL("image/png") + ")";
+     }
+
+    render() {
+        return (
+            <section id="welcome-section">
+            {this.generateNoise(1)}
+                <div className="welcome-container">
+                    <h1>TYLER PETERSON</h1>
+                    <h3>DEVELOPER</h3>
+                    <h3>DESIGNER</h3>
+                    <h3>TESTER</h3>
                 </div>
-            </div>
-        </section>
-    );
-  }
+                <div className="scroll-downs">
+                    <div className="mousey">
+                        <div className="scroller"></div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default WelcomeSection;
