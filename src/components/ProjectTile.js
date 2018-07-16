59d3bf6ef13c47f89e288e90b0b51663fa1aa6bb
TheNewStyles <tyler.peterson@utah.edu>
2018-07-16 15:35:13 -0500

    Add styling and animations to project tiles

--- src/components/ProjectTile.js
+++ src/components/ProjectTile.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
+
 import '../styles/ProjectTile.css';
 
 class ProjectsTile extends Component {
-  render() {
-    return (
-        <article className="project-tile">
-            <h4>{this.props.title}</h4>
-            <p>{this.props.info}</p>
-        </article>
-    );
-  }
+    render() {
+        return (
+            <article className="project-tile">
+                <Fade left>
+                    <h2>{this.props.title}</h2>
+                    <p>{this.props.info}</p>
+                </Fade>
+            </article>
+        );
+    }
 }
 
 export default ProjectsTile;
