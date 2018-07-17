ca42723423216b1d5111e96e4c97f2df5faebd5e
TheNewStyles <tyler.peterson@utah.edu>
2018-07-17 16:21:48 -0500

    Fix tile styles sizing

--- src/components/ProjectTile.js
+++ src/components/ProjectTile.js
@@ -4,14 +4,27 @@ import Fade from 'react-reveal/Fade';
 import '../styles/ProjectTile.css';
 
 class ProjectsTile extends Component {
+    constructor(props){
+        super(props);
+        this.screenSize = window.screen.width > 450;
+    }
+
     render() {
         return (
             <article className="project-tile">
-                <Fade left>
-                    <h2>{this.props.title}</h2>
-                    <img src={this.props.image} className="project-image" alt=""/>
-                    <p>{this.props.info}</p>
-                </Fade>
+                {this.screenSize ? (
+                    <Fade>
+                        <h2>{this.props.title}</h2>
+                            <img src={this.props.image} className="project-image" alt=""/>
+                        <p>{this.props.info}</p>
+                    </Fade>
+                ) : (
+                    <Fade left>
+                        <h2>{this.props.title}</h2>
+                            <img src={this.props.image} className="project-image" alt=""/>
+                        <p>{this.props.info}</p>
+                    </Fade>
+                )}
             </article>
         );
     }
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -17,8 +17,8 @@ class ProjectsSection extends Component {
                 <ProjectsTile title="PROJECT 4" image={image} info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " />
                 <ProjectsTile title="PROJECT 5" image={image} info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " />
                 <ProjectsTile title="PROJECT 6" image={image} info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " />
-                <ProjectsTile title="PROJECT 7" image={image} info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et "/>
-                <ProjectsTile title="PROJECT 8" image={image} info="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et "/>
+                <ProjectsTile title="Markdown Previewer" image={image} info="A Markdown previewer I created in React. Markdown is a lightweight markup language to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)"/>
+                <ProjectsTile title="React Docs Clone" image={image} info="While I was learning React I recreated the React documenation with React as a web design project."/>
             </div>
         </section>
     );
